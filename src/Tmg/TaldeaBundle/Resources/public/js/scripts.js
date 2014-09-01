758d6fa64f2a5a3b5ba59fa46b314adeb29bef42
xaaleja <xaaleja@gmail.com>
2014-09-02 00:55:37 +0200

    Iraganeko kontzertuen akordeoia guztiz bukatuta

--- src/Tmg/TaldeaBundle/Resources/public/js/scripts.js
+++ src/Tmg/TaldeaBundle/Resources/public/js/scripts.js
@@ -30,10 +30,17 @@ $(document).ready(function() {
   $(".fancybox").fancybox();
 
   $(".kontz-urtea").on("click", function(){
-    $(".kontz-urtea").siblings().children().next().slideUp("slow");
-    $(this).children().next().slideDown("slow");
-    console.log($(this).children());
-    console.log($(this).siblings().children().next());
+    console.log($(this));
+    console.log($(this).children().next().css('display'));
+    if($(this).children().next().css('display') == 'none')
+    {
+      $(".kontz-urtea").siblings().children().next().slideUp("slow");
+      $(this).children().next().slideDown("slow");
+    }
+    else
+    {
+      $(this).children().next().slideUp("slow");
+    }
   });
 
 });
